5cb5f8c800c59135f21b259cfc34b5065b9e457a
dk <dmi.kovaliov@gmail.com>
2018-08-24 13:02:33 +0300

    Revert "new commit"
    This reverts commit 2b1efdb

--- app/src/main/java/com/dk/safehome1/provider/WifiScan.java
+++ app/src/main/java/com/dk/safehome1/provider/WifiScan.java
@@ -1,24 +1,10 @@
 package com.dk.safehome1.provider;
 
-import android.app.Application;
-import android.content.Context;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiManager;
-
-import java.util.List;
-
 /**
  * Created by dk on 6/4/18.
  */
 
 public class WifiScan {
 
-    public static List<ScanResult> scan (Application context) {
-        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-
-      wifiManager.startScan();
-
-       return wifiManager.getScanResults();
-    }
 
 }
